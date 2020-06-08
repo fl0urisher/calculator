e21459d65420d24bd4d9e67f21e993920b364005
George Udonte <georgeudonte@gmail.com>
2020-06-08 10:52:22 +0100

    added more functions to js file

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,21 @@ function add(num1, num2){
     return num1 + num2;
 }
 
-num1 = parseInt(prompt("Please enter the first number_"));
-num2 = parseInt(prompt("Please enter the second number_"));
+function subtract(num1, num2){
+    return num1 - num2;
+}
 
-alert(add(num1, num2));
+function multiply(num1, num2){
+    return num1 * num2;
+}
+
+function divide(num1, num2){
+    return num1 / num2;
+}
+
+
+const num1 = parseInt(prompt("Please enter the first number_"));
+const num2 = parseInt(prompt("Please enter the second number_"));
+const answer = divide(num1, num2);
+
+alert(answer);
